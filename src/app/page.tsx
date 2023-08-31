f1c28076ccd2c1607201d0a957eef62406d0fb5d
Lucas Moreira <lucasguilheme.31@gmail.com>
2023-08-31 17:35:31 -0300

    fixed header and shortenLink display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import { Button } from "@/components/ui/button";
-import LinkItem from "@/features/home/Components/LinkItem";
-import UrlShortener from "@/features/home/Components/UrlShortener";
+
 import Link from "next/link";
 import { Suspense } from "react";
 import { cookies } from "next/headers";
 import LoggedHeader from "@/features/home/Components/LoggedHeader";
-import Image from "next/image";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
 import TabsContainer from "@/features/home/Components/TabsContainer";
+import Image from "next/image";
 
 export default async function Home() {
   const token = cookies().get("Wemely:Token");
@@ -31,21 +29,36 @@ export default async function Home() {
       {token ? (
         <LoggedHeader name={name} />
       ) : (
-        <nav className="w-full flex justify-end mt-4 ">
-          <Button variant={"ghost"} asChild>
-            <Link href="/login">Entrar</Link>
-          </Button>
-          <Button variant="ghost" asChild>
-            <Link href="/signup">Cadastre-se</Link>
-          </Button>
-        </nav>
+        <div className="flex justify-between items-center w-full  mt-4 ">
+          <Image
+            src="https://uploads-ssl.webflow.com/64777a8d9e690fab7ae929ff/649bd8ed4b3a895b4e593264_Group%2083.svg"
+            width={500}
+            height={300}
+            className="w-24"
+            alt="weme logo"
+          />
+          <nav className="w-full flex justify-end items-center">
+            <Button variant={"ghost"} asChild>
+              <Link href="/login">Entrar</Link>
+            </Button>
+            <Button variant="ghost" asChild>
+              <Link href="/signup">Cadastre-se</Link>
+            </Button>
+          </nav>
+        </div>
       )}
 
       <section className="flex flex-col justify-center max-w-6xl w-full flex-grow ">
-        <Suspense>
-          {/* <UrlShortener /> */}
-          <TabsContainer links={links} />
-        </Suspense>
+        {!token ? (
+          <p className=" text-xl text-center">
+            Entre na sua conta para poder encurtar links!
+          </p>
+        ) : (
+          <Suspense>
+            {/* <UrlShortener /> */}
+            <TabsContainer links={links} />
+          </Suspense>
+        )}
       </section>
     </main>
   );
